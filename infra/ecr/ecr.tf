8a353729107d02cd48eff37c085b6ccf824b5638
chandrapowerstar <30374208+chandrapowerstar@users.noreply.github.com>
2020-02-02 18:08:42 +0530

    Update ecr.tf

--- infra/ecr/ecr.tf
+++ infra/ecr/ecr.tf
@@ -1,1 +1,20 @@
 
+data "aws_caller_identity" "current" {}
+
+terraform {
+  backend "s3" {
+    dynamodb_table = "terraform-state-lock"
+    bucket         = "966270299484-terraform-state"
+    key            = "common/ecr-ui-app.tfstate"
+    region         = "eu-west-1"
+    profile        = "pollen-nonprod"
+  }
+}
+
+module "ecr_repo" {
+  source        = "git::git@github.com:SainsburysNDA/pollen-infra-common.git//terraform-alb-ecs/modules/ecr-repo"
+  ecr_repo_name = var.ecr_repo_name
+}
+
+
+variable "ecr_repo_name" {}
